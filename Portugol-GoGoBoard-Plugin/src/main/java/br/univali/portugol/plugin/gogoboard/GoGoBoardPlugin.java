e38c9f2208e94a4cc872cec3fccaa59f5a7f8794
Adsu <shinadson@gmail.com>
2018-07-27 14:48:16 -0300

    Alterando chamada de ação

--- Portugol-GoGoBoard-Plugin/src/main/java/br/univali/portugol/plugin/gogoboard/GoGoBoardPlugin.java
+++ Portugol-GoGoBoard-Plugin/src/main/java/br/univali/portugol/plugin/gogoboard/GoGoBoardPlugin.java
@@ -6,6 +6,7 @@ import br.univali.portugol.plugin.gogoboard.acoes.AcaoExibirMonitor;
 import br.univali.portugol.plugin.gogoboard.biblioteca.GoGoBoard;
 import br.univali.portugol.plugin.gogoboard.componetes.DispositivoGoGo;
 import br.univali.portugol.plugin.gogoboard.driver.GoGoDriver;
+import br.univali.ps.plugins.base.GerenciadorPlugins;
 import br.univali.ps.plugins.base.Plugin;
 import br.univali.ps.plugins.base.UtilizadorPlugins;
 import br.univali.ps.plugins.base.VisaoPlugin;
@@ -35,9 +36,12 @@ public final class GoGoBoardPlugin extends Plugin {
     @Override
     protected void inicializar(UtilizadorPlugins utilizador) {
         this.utilizador = utilizador;
-        this.utilizador.instalarAcaoPlugin(this, new AcaoExibirMonitor(this));
-        this.utilizador.instalarAcaoPlugin(this, new AcaoExibirLogo(this));
-        this.utilizador.instalarAcaoPlugin(this, new AcaoEnviarByteCode(this));
+//        this.utilizador.instalarAcaoPlugin(this, new AcaoExibirMonitor(this));
+//        this.utilizador.instalarAcaoPlugin(this, new AcaoExibirLogo(this));
+//        this.utilizador.instalarAcaoPlugin(this, new AcaoEnviarByteCode(this));
+        GerenciadorPlugins.getInstance().instalarAcaoPlugin(this, new AcaoExibirMonitor(this));
+        GerenciadorPlugins.getInstance().instalarAcaoPlugin(this, new AcaoExibirLogo(this));
+        GerenciadorPlugins.getInstance().instalarAcaoPlugin(this, new AcaoEnviarByteCode(this));
         this.utilizador.registrarBiblioteca(GoGoBoard.class);
         super.inicializar(utilizador);
     }
